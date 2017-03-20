e7f9071f07e0aed40e21027b2bd61ab59ac2d20c
zhangzaidong <zhangzaidong@lianluo.com>
2017-03-21 00:13:09 +0800

    Home

--- app/src/main/java/com/amibtion/mvp/reader/module/home/HomeActivity.java
+++ app/src/main/java/com/amibtion/mvp/reader/module/home/HomeActivity.java
@@ -1,15 +1,188 @@
 package com.amibtion.mvp.reader.module.home;
 
+import android.Manifest;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.amibtion.mvp.reader.R;
+import com.amibtion.mvp.reader.module.base.BaseActivity;
+import com.amibtion.mvp.reader.utils.SnackbarUtils;
+import com.dl7.downloaderlib.FileDownloader;
+import com.tbruyelle.rxpermissions.RxPermissions;
 
-public class HomeActivity extends AppCompatActivity {
+import java.io.File;
+
+import butterknife.BindView;
+import okio.Buffer;
+import rx.functions.Action1;
+
+/**
+ * 程序入口
+ */
+public class HomeActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.fl_container)
+    FrameLayout mFrameLayout;
+    @BindView(R.id.nav_view)
+    NavigationView mNavView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    private RxPermissions mRxPermission;
+
+    private SparseArray<String> mSparseTags = new SparseArray<>();
+    private long mExitTime = 0;
+    private int mItemId = -1;
+
+    private Handler mHander = new Handler(new Handler.Callback() {
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what){
+                case R.id.nav_news:
+                    replaceFragment(R.id.fl_container,new NewsMainFragment(),mSparseTags.get(R.id.nav_news));
+                    break;
+                case R.id.nav_photos:
+                    replaceFragment(R.id.fl_container,new PhotoMainFragment(),mSparseTags.get(R.id.nav_photos));
+                    break;
+                case R.id.nav_videos:
+                    replaceFragment(R.id.fl_container,new VideoMainFragment(),mSparseTags.get(R.id.nav_videos));
+                    break;
+                case R.id.nav_setting:
+                    SettingsActivity.launch(HomeActivity.this);
+                    break;
+            }
+            mItemId = -1;
+            return true;
+        }
+    });
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int attachLayoutRes() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initInjector() {
+
+    }
+
+    @Override
+    protected void initViews() {
+        _initDrawerLayout(mDrawerLayout,mNavView);
+        mSparseTags.put(R.id.nav_news,"News");
+        mSparseTags.put(R.id.nav_photos,"Photos");
+        mSparseTags.put(R.id.nav_videos,"Videos");
+        _getPemission();
+    }
+
+    @Override
+    protected void updateViews(boolean isRefresh) {
+        mNavView.setCheckedItem(R.id.nav_news);
+        addFragment(R.id.fl_container,new NewsMainFragment(),"News");
+    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_home);
+//    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        if (item.isChecked()){
+            return true;
+        }
+        mItemId = item.getItemId();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        final int stackEntryCount = getSupportFragmentManager().getBackStackEntryCount();
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else if (stackEntryCount == 1) {
+            _exit();
+        } else {
+            final String tagName = getSupportFragmentManager().getBackStackEntryAt();
+            mNavView.setCheckedItem(mSparseTags.keyAt(mSparseTags.indexOfValue(tagName)));
+            super.onBackPressed();
+
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            mDrawerLayout.openDrawer(GravityCompat.START);
+            return false;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void _initDrawerLayout(DrawerLayout drawerLayout,NavigationView navView){
+        if (Build.VERSION.SDK_INT > = Build.VERSION_CODES.KITKAT){
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+            //将侧边栏顶部延伸至status bar
+            drawerLayout.setFitsSystemWindows(true);
+            //将主页面顶部延伸至status bar
+            drawerLayout.setClipToPadding(false);
+        }
+
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                mHander.sendEmptyMessage(mItemId);
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+    }
+
+    private void _getPemission() {
+        final File dir = new File(FileDownloader.getDownloadDir());
+        if(!dir.exists() || !dir.isDirectory()){
+            dir.delete();
+            new RxPermissions(this)
+                    .request(Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE)
+                    .subscribe(new Action1<Boolean>() {
+                        @Override
+                        public void call(Boolean granted) {
+                            if(granted){
+                                dir.mkdirs();
+                            } else {
+                                SnackbarUtils.showSnackbar(HomeActivity.this,"下载目录创建失败",true);
+                            }
+                        }
+                    });
+        }
     }
 }
